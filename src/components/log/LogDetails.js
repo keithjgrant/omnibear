dfae97b3285b74aea71f9a95e5520458dd51961f
Keith J. Grant <barkeep@keithjgrant.com>
2022-11-15 12:31:01 -0800

    fix logging of object with null properties; add description

--- src/components/log/LogDetails.js
+++ src/components/log/LogDetails.js
@@ -1,8 +1,8 @@
-import {h, Component} from 'preact';
+import { h, Component } from 'preact';
 
 export default class LogDetails extends Component {
   render() {
-    const {details} = this.props;
+    const { details } = this.props;
     return (
       <div className="log-details">{this.renderDetail(details, true)}</div>
     );
@@ -10,15 +10,15 @@ export default class LogDetails extends Component {
 
   renderDetail(detail, isTopLevel) {
     const marginLeft = `${isTopLevel ? 0 : 1}em`;
-    if (typeof detail === 'string' || typeof detail === 'number') {
+    if (!detail || typeof detail === 'string' || typeof detail === 'number') {
       return <span>{detail}</span>;
     }
     if (Array.isArray(detail)) {
       return (
-        <div style={{marginLeft}}>
+        <div style={{ marginLeft }}>
           {'['}
           {detail.map((d, i) => (
-            <div key={i} style={{marginLeft}}>
+            <div key={i} style={{ marginLeft }}>
               {this.renderDetail(d)},
             </div>
           ))}
@@ -28,9 +28,9 @@ export default class LogDetails extends Component {
     }
     return [
       <span key="open-brace">{'{'}</span>,
-      <div key="content" style={{marginLeft}}>
-        {Object.keys(detail).map(key => (
-          <div key={key} style={{marginLeft: '1em'}}>
+      <div key="content" style={{ marginLeft }}>
+        {Object.keys(detail).map((key) => (
+          <div key={key} style={{ marginLeft: '1em' }}>
             {key}: {this.renderDetail(detail[key])}
           </div>
         ))}
